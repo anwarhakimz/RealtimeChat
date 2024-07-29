6de5ce19140296d1f991b99fb5edbfe68b88ad7a
anwarhakim31 <anwarhakim31@gmail.com>
2024-07-29 07:12:32 +0700

    deploy

--- index.js
+++ index.js
@@ -34,6 +34,34 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/channel", channelRoutes);
 app.use(errorMiddleware);
 
+app.get("/", (req, res) => {
+  res.send(`
+    <html">
+      <head>
+        <title>API Status</title>
+        <script>
+          function updateTime() {
+            const date = new Date();
+            const idn = new Intl.DateTimeFormat('id-ID', {
+              dateStyle: 'full',
+              timeStyle: 'long',
+              timeZone: 'Asia/Jakarta',
+            }).format(date);
+            document.getElementById("timestamp").textContent = idn;
+          }
+          setInterval(updateTime, 1000);
+        </script>
+      </head>
+      <body style="font-family: Arial, sans-serif; background:#999999; color:#000000; text-align: center; width:100%; height:100vh; display:flex; justify-content:center; align-items:center; overflow:hidden;">
+        <h1 ">API is running</h1>
+        <p ">Status: <strong>success</strong></p>
+        <p ">Timestamp: <strong id="timestamp">${new Date().toISOString()}</strong></p>
+        <img src="https://firebasestorage.googleapis.com/v0/b/android-7c309.appspot.com/o/111111212121.png?alt=media&token=1dba2219-a414-4758-b4b6-a0fbb2f1b75f" alt="API Image" style="width:100px; height:100px; margin-top: 20px;">
+      </body>
+    </html>
+  `);
+});
+
 const connectDB = async () => {
   try {
     await mongoose.connect(databaseURL);
